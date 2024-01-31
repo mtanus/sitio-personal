7f982084c0286eec0357157482f60ab181adfe55
Mauricio <mtanus@users.noreply.github.com>
2024-01-31 18:07:28 +0000

    Inicio de sitio web

---
+++ js/principal.js
@@ -0,0 +1,26 @@
+/* Contraer y expandir menú del header */
+let menuHamburguesa = document.querySelector('#logo-menu-hamburguesa');
+let menuCerrar = document.querySelector('#logo-menu-cerrar');
+let contenedorListadoMenu = document.querySelector('#contenedor-listado-menu');
+
+menuHamburguesa.addEventListener('click', () => {
+    contenedorListadoMenu.classList.remove('elemento-oculto');
+    contenedorListadoMenu.classList.add('elemento-visible');
+
+    menuHamburguesa.classList.remove('elemento-visible');
+    menuHamburguesa.classList.add('elemento-oculto');
+
+    menuCerrar.classList.remove('elemento-oculto');
+    menuCerrar.classList.add('elemento-visible');
+})
+
+menuCerrar.addEventListener('click', () => {
+    contenedorListadoMenu.classList.remove('elemento-visible');
+    contenedorListadoMenu.classList.add('elemento-oculto');
+
+    menuHamburguesa.classList.remove('elemento-oculto');
+    menuHamburguesa.classList.add('elemento-visible');
+
+    menuCerrar.classList.remove('elemento-visible');
+    menuCerrar.classList.add('elemento-oculto');
+})
